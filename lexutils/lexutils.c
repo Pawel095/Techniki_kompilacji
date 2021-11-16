5ab79f15c196f9cd2875245efe5113aa49b7211d
Paweł 'Andrzej' Pankiewicz <pawel.pankiewicz@xdsa.pl>
2021-11-16 19:34:16 +0100

    space for code that wont fit in flex source

---
+++ lexutils/lexutils.c
@@ -0,0 +1,9 @@
+#include "global.h"
+int handle_alnum(char lexbuf[])
+{
+    int p = lookup(lexbuf);
+    if (p == 0)
+        p = insert(lexbuf, ID);
+    tokenval = p;
+    return symtable[p].token;
+}
